a57adb0554dd621898291d7522e328fe212d1996
Steppio <steppio.cosentino@gmail.com>
2022-05-16 16:42:27 +0200

    .

--- lib/flistfood_order.dart
+++ lib/flistfood_order.dart
@@ -5,3 +5,11 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+
+class FlistFoodOrder {
+  int instance = 0;
+
+  void addOne() => instance + 1;
+
+  void removeOne() => instance - 1;
+}
